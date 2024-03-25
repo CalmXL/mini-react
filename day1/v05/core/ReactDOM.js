d4141afafc2217c3a1af7923a30f44ffb79857ea
xl <xl18845898715@gmail.com>
2024-03-25 21:41:55 +0800

    fix: render move React

--- day1/v05/core/ReactDOM.js
+++ day1/v05/core/ReactDOM.js
@@ -1,45 +1,15 @@
 import React from "./React.js";
-const TEXT_ELEMENT_TYPE = "TEXT_ELEMENT";
-
-function _render(vdom, container) {
-  // 1. 创建对应真实 DOM
-  const dom =
-    vdom.type === TEXT_ELEMENT_TYPE
-      ? document.createTextNode("")
-      : document.createElement(vdom.type);
-
-  // 2. 处理 非 children 的 props
-  Object.keys(vdom.props).forEach((key) => {
-
-    if (key !== "children") {
-      dom[key] = vdom.props[key];
-    }
-  });
-
-  // 3. 存在 children 处理 chilren
-  const children = vdom.props.children;
-  if (children) {
-    children.forEach((child) => {
-      typeof child === 'string'
-                   ? _render(React.createTextElement(child), dom) 
-                   : _render(child, dom);
-    });
-  }
-
-  // 4. 真实DOM渲染到页面
-  container.append(dom);
-}
 
 function createRoot(container) {
   const render = (el) => {
-    _render(el, container)
-  }
+    React.render(el, container);
+  };
 
   return {
-    render
-  }
+    render,
+  };
 }
 
 export default {
-  createRoot
-}
+  createRoot,
+};
